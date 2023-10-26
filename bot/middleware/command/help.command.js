fd36dc8d6d47cf28d6f1cbda2221141823ef7a23
Artem L <vlossaa@gmail.com>
2023-10-26 12:04:35 +0700

    init project

---
+++ bot/middleware/command/help.command.js
@@ -0,0 +1,10 @@
+const { Markup } = require("telegraf");
+const bot = require("../../connection/token.connection");
+
+module.exports = bot.help(async (ctx) => {
+    try {
+        return ctx.reply("How can I help you?");
+    } catch (e) {
+        console.log(e);
+    }
+});
---
+++ bot/middleware/command/setting.command.js
@@ -0,0 +1,43 @@
+const { Markup } = require("telegraf");
+const bot = require("../../connection/token.connection");
+
+module.exports = bot.settings(async (ctx) => {
+    try {
+        return ctx.reply("Here you can configure the bot!");
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+// const { Markup } = require("telegraf");
+// const bot = require("../../connection/token.connection");
+// const db = require("../../connection/db.connection");
+// const UserModel = require("../../model/user.model");
+
+// module.exports = bot.settings(async (ctx) => {
+//     try {
+//         // await db.sync();
+
+//         const startPayload = ctx.startPayload;
+//         const chatID = ctx.chat.id;
+//         const firstName = ctx.chat.first_name;
+//         const lastName = ctx.chat.last_name;
+//         const username = ctx.chat.username;
+
+//         // const foundUser = await UserModel.findOne({where:{chatID:ctx.chat.id}});
+//         // if (!foundUser) {
+//         //    await UserModel.create({
+//         //       chatID: chatID,
+//         //       firstName: firstName,
+//         //       lastName: lastName,
+//         //       username: username,
+//         //       admin: false,
+//         //       startPayload: startPayload
+//         //    });
+//         // }
+
+//         return ctx.replyWithHTML(`Hi, <b>${firstName}</b>!`);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// });
